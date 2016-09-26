cfd991ade1055d07923c9f64cb393bbd91ac6574
EdgarCoronaRD <edgarcoron101@gmail.com>
2016-09-26 09:47:44 -0500

    hola hola

---
+++ programa1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{ //llaves
+
+
+cout<<"tamaño del int es:"<<sizeof(int)<<" bytes \n";
+cout<<"tamaño del short es:"<<sizeof(short)<<" bytes \n";
+cout<<"tamaño del long es:"<<sizeof(long)<<" bytes \n";
+cout<<"tamaño del char es:"<<sizeof(char)<<" bytes \n";
+cout<<"tamaño del bool es:"<<sizeof(bool)<<" bytes \n";
+cout<<"tamaño del float es:"<<sizeof(float)<<" bytes \n";
+cout<<"tamaño del double es:"<<sizeof(double)<<" bytes \n";
+cout<<"gracias totales culeros:";
+return 0;
+
+}
