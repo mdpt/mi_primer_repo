7db3ebfdc3e13623c8499943229435c7b26c0170
caren99 <thekarenzoe@gmail.com>
2016-09-26 09:52:55 -0500

    bye

--- programa1.cpp
+++ programa1.cpp
@@ -11,7 +11,9 @@ cout<<"tamaño del char es:"<<sizeof(char)<<" bytes \n";
 cout<<"tamaño del bool es:"<<sizeof(bool)<<" bytes \n";
 cout<<"tamaño del float es:"<<sizeof(float)<<" bytes \n";
 cout<<"tamaño del double es:"<<sizeof(double)<<" bytes \n";
-cout<<"gracias totales culeros:";
+cout<<"gracias totales valeros:";
+cout<<"tamaño del int es:"<<sizeof(int)<<" bytes \n";
+cout<<"tamaño del short es:"<<sizeof(short)<<" bytes \n";
 return 0;
 
 }
